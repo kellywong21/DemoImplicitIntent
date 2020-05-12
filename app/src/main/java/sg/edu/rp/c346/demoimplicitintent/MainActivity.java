cddb5d8ead9dc436b70346a722d06bea3af897d6
18004885 <18004885@myrp.edu.sg>
2020-05-12 11:15:03 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoimplicitintent/MainActivity.java
@@ -0,0 +1,22 @@
+package sg.edu.rp.c346.demoimplicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       
+    }
+}
